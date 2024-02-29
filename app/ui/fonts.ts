a7ed1e2f6fc421293a2743f9e6016bd22b321ef9
Jhonatan Giraldo <jhonatangiraldo111@gmail.com>
2024-02-29 16:24:46 -0500

    Chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+// text-xl text-gray-800 md:text-3xl md:leading-normal __className_712214
+// text-xl text-gray-800 md:text-3xl md:leading-normal __className_6fb9c5
